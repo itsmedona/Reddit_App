cfc065259989117762efcf0972522425bc5264ad
itsmedona <donashaji254@gmail.com>
2024-04-02 14:33:16 +0530

    ....

--- lib/presentation/ProfileScreen/view/ProfileScreen.dart
+++ lib/presentation/ProfileScreen/view/ProfileScreen.dart
@@ -115,7 +115,6 @@ class ProfileScreen extends StatelessWidget {
                 ),
               ],
             ),
-            //Expanded(child: )
           ],
         ),
       ),
--- lib/presentation/home_screen/view/home_screen.dart
+++ lib/presentation/home_screen/view/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:clone_app/repository/drawer_widget.dart';
-import 'package:clone_app/repository/home_screen_widget.dart';
+import 'package:clone_app/repository/Post(HomeScreenWidget).dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
