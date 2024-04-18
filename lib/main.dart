18b31694e527cbd28f3fe96bdf996d55bb10fa2a
najeebnaju512 <najeebnaju512@gmail.com>
2024-04-18 16:40:02 +0530

    login and register2

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,9 @@
 import 'package:clone_app/presentation/FirstScreen/view/first_screen.dart';
 import 'package:clone_app/presentation/login/controller/login_controller.dart';
-//import 'package:clone_app/presentation/about_you_screen/view/about_you_screen.dart';
-//import 'package:clone_app/presentation/chose_username/view/username_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'presentation/registration/controller/registration_controller.dart';
-
-//import 'presentation/interests_screen/view/interests_screen.dart';
-
-// void main() {
-//   runApp(MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return const MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: FirstScreen(),
-//     );
-//   }
-// }
-
 
 void main() {
   runApp(MultiProvider(
